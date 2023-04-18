cc3dc1989fc8a40712a5e42d33efec6f3580f7ef
SeoIHyeon <tjwn1408@naver.com>
2023-04-18 16:08:34 +0900

    [BOJ] 좋다 / 골드 4
    https://www.acmicpc.net/problem/1253

--- I-HYEON/4월 목표/BOJ_1253_좋다.py
+++ I-HYEON/4월 목표/BOJ_1253_좋다.py
@@ -1,18 +1,30 @@
-from itertools import combinations
-
 N = int(input())
 lst = list(map(int, input().split()))
 
 lst.sort()  # 크기 순으로 정렬한다
 
 Good = 0
+
 for i in range(N):
-    if i >= 2:
-        temp_lst = lst[::]
-        temp_lst.pop(i)
-        Good_list = list(set(map(lambda x:sum(x),list(combinations(temp_lst, 2)))))
-        print(Good_list)
-        if lst[i] in Good_list:
-            print(lst[i])
+    left = 0
+    right = N-1
+
+    while left < right:
+        if left == i:
+            left += 1
+            continue
+        if right == i:
+            right -= 1
+            continue
+
+        summ = lst[left] + lst[right]
+
+        if summ == lst[i]:
             Good += 1
+            break
+        elif summ < lst[i]:
+            left += 1
+        else:
+            right -= 1
+
 print(Good)
